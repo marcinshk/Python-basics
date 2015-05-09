b225354bbada7f3e457008dcb68f0ed839393ea7
Marcin Szarek <marcinshk@gmail.com>
2015-05-09 19:28:11 +0200

    func4_add modified and now used by play-try-test.py

--- my_own_python_module.py
+++ my_own_python_module.py
@@ -11,7 +11,12 @@ def func3():
     print "This has been developed on branch: dev_func3. Review and merge to branch dev and to master."
     return 0
 
-def func4():
-    print "Developed on branch dev_func4."
+def func4_add(a_num1, a_num2):
+    """
+    This function takes two args and adds them.
+    Return value: the result of the addition.
+    Developed on branch dev_func4.
+    """
+    return a_num1 + a_num2
 
 # EOF
--- play-try-test.py
+++ play-try-test.py
@@ -9,6 +9,8 @@ def main():
     """
     This file is for testing how stuff works in Python.
     """
+
+    print "func4_add returned: %d" % my_own_python_module.func4_add(2,3)
     
     print "INFO: main() finished"
 
