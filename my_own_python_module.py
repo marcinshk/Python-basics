e19714832e36192835b6837d778795d932a3d9a0
Marcin Szarek <marcinshk@gmail.com>
2015-05-09 16:43:40 +0200

    Let's start learning

---
+++ my_own_python_module.py
@@ -0,0 +1,7 @@
+#! python
+
+def func1():
+    print "I'm just learning how to create Python modules"
+    return 0
+
+# EOF
---
+++ play-try-test.py
@@ -0,0 +1,19 @@
+#! python
+
+import sys
+sys.path.append('./')
+
+import my_own_python_module
+
+def main():
+    """
+    This file is for testing how stuff works in Python.
+    """
+    
+    print "INFO: main() finished"
+
+#-------------------------------------------------------
+
+main()
+
+# EOF
