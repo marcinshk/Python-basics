c1a8a725dd1e1f7d8cb8c8de8b8da1ed54becc14
Marcin Szarek <marcinshk@gmail.com>
2015-05-09 19:19:14 +0200

    Created func4 on branch dev_func4.

--- my_own_python_module.py
+++ my_own_python_module.py
@@ -1,5 +1,4 @@
-#! python
-
+#! python 
 def func1():
     print "I'm just learning how to create Python modules"
     return 0
@@ -11,5 +10,8 @@ def func2():
 def func3():
     print "This has been developed on branch: dev_func3. Review and merge to branch dev and to master."
     return 0
+
+def func4():
+    print "Developed on branch dev_func4."
 
 # EOF
