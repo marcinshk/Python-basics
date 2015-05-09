b0fb283f25822d9b744f66fc6e2088efa8f54dd1
Marcin Szarek <marcinshk@gmail.com>
2015-05-09 18:06:00 +0200

    Created func3().

--- my_own_python_module.py
+++ my_own_python_module.py
@@ -4,4 +4,8 @@ def func1():
     print "I'm just learning how to create Python modules"
     return 0
 
+def func3():
+    print "This has been developed on branch: dev_func3. Review and merge to branch dev and to master."
+    return 0
+
 # EOF
