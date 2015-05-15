e3ae6c70a2483b1b24386eed63c28009f7b998fb
U-CISCO\marcszar <marcszar@MARCSZAR-WS01.cisco.com>
2015-05-15 14:49:13 +0200

    Just playing...

--- ObjectOrientedProgramming.py
+++ ObjectOrientedProgramming.py
@@ -1,1 +1,16 @@
 #! python
+
+
+class Parent(object):
+  def implicit(self):
+    print "PARENT implicit()"
+
+class Child(Parent):
+  pass
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
+ 
